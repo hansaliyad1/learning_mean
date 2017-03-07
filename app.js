4aff38af8a070cfd2cb6c34910ae5238ba666f20
Dharmang Patel <dharmangp25@gmail.com>
2017-03-07 18:49:50 -0500

    Getting User Input, Enable Upvoting, Submitting Link

--- app.js
+++ app.js
@@ -6,12 +6,25 @@ var app = angular.module('flapperNews', []);
 app.controller('MainCtrl', [
     '$scope',
     function ($scope) {
+        $scope.test = "Hello World";
+
         $scope.posts = [
-            'post 1',
-            'post 2',
-            'post 3',
-            'post 4',
-            'post 5'
+            {title: 'post 1', upvotes: 5},
+            {title: 'post 2', upvotes: 2},
+            {title: 'post 3', upvotes: 15},
+            {title: 'post 4', upvotes: 9},
+            {title: 'post 5', upvotes: 4}
         ];
+
+        $scope.addPost = function () {
+            if(!$scope.title || $scope.title === '') {return;}
+            $scope.posts.push({title: $scope.title, link: $scope.link, upvotes: 0});
+            $scope.title = '';
+            $scope.link = '';
+        };
+
+        $scope.incrementUpvotes = function (post) {
+            post.upvotes += 1;
+        };
     }
 ]);
