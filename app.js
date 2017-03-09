fe88327d3eb30b61aeb2fb2b56cbe6c76f18e7e9
Dharmang Patel <dharmangp25@gmail.com>
2017-03-09 11:47:13 -0500

    I completed tutorial till we start Nodejs. I have some errors in project. I am still solving it

--- app.js
+++ app.js
@@ -1,24 +1,40 @@
 /**
  * Created by hansa on 3/7/2017.
  */
-var app = angular.module('flapperNews', []);
+var app = angular.module('flapperNews', ['ui.router']);
 
-app.controller('MainCtrl', [
-    '$scope',
-    function ($scope) {
-        $scope.test = "Hello World";
-
-        $scope.posts = [
+app.factory('posts', [function () {
+    var o = {
+        posts: [
             {title: 'post 1', upvotes: 5},
             {title: 'post 2', upvotes: 2},
             {title: 'post 3', upvotes: 15},
             {title: 'post 4', upvotes: 9},
             {title: 'post 5', upvotes: 4}
-        ];
+        ]
+    };
+    return o;
+}]);
+
+app.controller('MainCtrl', [
+    '$scope',
+    'posts',
+    function ($scope, posts) {
+        $scope.test = "Hello World";
+
+        $scope.posts = posts.posts;
 
         $scope.addPost = function () {
             if(!$scope.title || $scope.title === '') {return;}
-            $scope.posts.push({title: $scope.title, link: $scope.link, upvotes: 0});
+            $scope.posts.push({
+                title: $scope.title,
+                link: $scope.link,
+                upvotes: 0,
+                comments: [
+                    {author: 'Joe', body: 'Cool posts!', upvotes: 0},
+                    {author: 'Bob', body: 'Great idea but everything is wrong!', upvotes: 0}
+                ]
+            });
             $scope.title = '';
             $scope.link = '';
         };
@@ -26,5 +42,43 @@ app.controller('MainCtrl', [
         $scope.incrementUpvotes = function (post) {
             post.upvotes += 1;
         };
+    }
+]);
+
+app.controller('PostsCtrl', [
+    '$scope',
+    '$stateParams',
+    'posts',
+    function ($scope, $stateParams, posts) {
+        $scope.post = posts.posts[$stateParams.id];
+    
+        $scope.addComment = function () {
+            if ($scope.body === ''){return;}
+            $scope.post.comments.push({
+                body: $scope.body,
+                author: 'user',
+                upvotes: 0
+            });
+            $scope.body = '';
+        }
+    }
+]);
+
+app.config([
+    '$stateProvider',
+    '$urlRouterProvider',
+    function ($stateProvider, $urlRouterProvider) {
+        $stateProvider
+            .state('home', {
+                url: '/home',
+                templateUrl: '/home.html',
+                controller: 'MainCtrl'
+            })
+            .state('posts', {
+                url: '/posts/{id}',
+                templateUrl: '/posts.html',
+                controller: 'PostCtrl'
+            })
+        $urlRouterProvider.otherwise('home');
     }
 ]);
